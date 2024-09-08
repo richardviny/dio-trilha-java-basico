8937df6695ddafc02ef6d2e0563dc2521ea1667d
Richard Vinicius <richardvinycius@hotmail.com>
2024-09-08 11:30:48 -0300

    ajeitando o java

--- meu-primeiro-java/src/OperadoresPart2.java
+++ meu-primeiro-java/src/OperadoresPart2.java
@@ -5,9 +5,15 @@ public class OperadoresPart2 {
 
        // menorque < > maiorque  != sempre vai dar o booleano
         boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10.0;
         System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
 
         
     }
 }
-}
+
