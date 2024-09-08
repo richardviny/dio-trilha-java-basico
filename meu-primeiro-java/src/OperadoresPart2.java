e7369fb15b655a973c2e2a6a968088992e4a45f8
Richard Vinicius <richardvinycius@hotmail.com>
2024-09-08 10:52:16 -0300

    part2

---
+++ meu-primeiro-java/src/OperadoresPart2.java
@@ -0,0 +1,13 @@
+public class OperadoresPart2 {
+    public static void main(String[] args) {
+       int resto = 21 % 7;
+       System.out.println(resto);
+
+       // menorque < > maiorque  != sempre vai dar o booleano
+        boolean isDezMaiorQueVinte = 10 > 20;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+
+        
+    }
+}
+}
