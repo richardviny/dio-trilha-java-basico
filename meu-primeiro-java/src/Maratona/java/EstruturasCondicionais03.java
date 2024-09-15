a5f8f2d76b73046ebeba04c85d9f98d5190f6db9
Richard Vinicius <richardvinycius@hotmail.com>
2024-09-15 02:12:44 -0300

    erro pc

---
+++ meu-primeiro-java/src/Maratona/java/EstruturasCondicionais03.java
@@ -0,0 +1,13 @@
+package Maratona.java;
+
+public class EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario = 6000;
+        String mensagemDoar= "Eu vou doar 500 a instituição";
+        String mensagemNaoDOar = "Ainda não tenho dinheiro .";
+        //(condicao) ? verdadeiro : falso
+        String resultado = salario > 5000 ? " Eu vou doar 500 a instituição" : "Ainda não tenho dinheiro";
+        boolean possoDoar = salario > 5000;
+      
+}
+}
