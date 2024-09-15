9275ca47d7285be4a84a0a5f32b5f27af4b673fe
Richard Vinicius <richardvinycius@hotmail.com>
2024-09-15 02:19:47 -0300

    1

--- meu-primeiro-java/src/Maratona/java/introducao/estruturasDeRepeticao.java
+++ meu-primeiro-java/src/Maratona/java/introducao/estruturasDeRepeticao.java
@@ -1,8 +1,17 @@
 package Maratona.java.introducao;
 // while, do while, for 
 public class estruturasDeRepeticao {
-    int count = 0;
-    while(count < 10){
-        System.out.println(count);
+    public static void main(String[] args) {
+        int count = 0;
+
+        // Loop while
+        while (count < 10) {
+            System.out.println(++count);
+        }
+
+        // Loop do-while
+        do {
+            System.out.println("dentro do do-while");
+        } while (count < 10);
     }
 }
